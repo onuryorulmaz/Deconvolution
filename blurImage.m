e6c0428b0947cef50b30dc1baf0e9b86298a866b
onuryorulmaz <yorulmazonur@gmail.com>
2017-07-01 16:06:33 +0300

    bugfix

--- blurImage.m
+++ blurImage.m
@@ -1,12 +1,7 @@
 function [ y, img ] = blurImage( img , h )
-%BLURIMAGE Summary of this function goes here
-%   Detailed explanation goes here
 
 padsize = 20;
 img = padarray(img, [padsize, padsize], 'replicate', 'both');
-img = padarray(img, 50*[1, 1], 'both');
-
-%h = fspecial('motion', 12, 5);
 
 stdDev = 0.01;
 
@@ -15,5 +10,7 @@ y = conv2(img,h,'same');
 noise = stdDev * randn(size(y));
 y = y + noise;
 
+img=img(1+padsize:end-padsize, 1+padsize:end-padsize);
+y=y(1+padsize:end-padsize, 1+padsize:end-padsize);
 end
 
